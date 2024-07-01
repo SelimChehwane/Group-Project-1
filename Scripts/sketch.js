ca66215c253e166e2aa35ead45dd6406de6cbb31
BasselAlShaar <basselalshaar11@gmail.com>
2024-07-01 12:58:35 +0300

    Configured Phaser in sketch.js and added the images of the landing page in assets folder as well as created landingPage.js

--- Scripts/sketch.js
+++ Scripts/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(400, 400);
-}
 
-function draw() {
-  background(220);
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x00FF00,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  physics:{
+    default: 'arcade',
+    arcade: {
+      gravity: {y:200},
+    }
+  },
+  
 }
+const game = new Phaser.Game(config);
+
+
